d13b7fd77a661016d1728881999acdb399211c03
Shubham Sharma <shubham49412@gmail.com>
2026-03-27 17:58:00 +0530

    fix: resolved intermittent 500 errors by enabling SQLAlchemy pool_pre_ping

--- app/__init__.py
+++ app/__init__.py
@@ -24,6 +24,10 @@ def create_app():
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, '..', 'app.db')
         
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+        "pool_pre_ping": True,
+        "pool_recycle": 300,  # Recycle connections every 5 minutes
+    }
 
     # Setup DB
     db.init_app(app)
